b7f2ba739a1079c564b9b1249f918b4fc7b053f3
Jose Antonio Morales Gutierrez <josea.morgut.1@educa.jcyl.es>
2026-01-21 13:20:03 +0100

    Ejercicio sobre HastSet, listas, bucles y duplicados

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public  class Main {
+    public static void main(String[] args) {
+        //Declaramos un scanner
+        Scanner sc = new Scanner(System.in);
+        //Creamos una lista de string listaPalabras
+        List<String> listaPalabras = new ArrayList<>();
+        //Le pedimos al usuario que introduzca palabras, si escribe 'fin' termina el programa
+        System.out.println("Introduce palabras (escribe 'fin' para terminar):");
+
+        //Declaramos una variable palabra
+        String palabra;
+
+        //A traves de un bucle do-while le el usuario escribe palabras y las añade a listaPalabras
+        do {
+            palabra = sc.next();
+            if (!palabra.equalsIgnoreCase("fin")) {
+                listaPalabras.add(palabra);
+            }
+        } while (!palabra.equalsIgnoreCase("fin"));
+
+        //Creamos un HastSet de palabras con el contenido de listaPalabras
+        HashSet<String> palabras = new HashSet<>(listaPalabras);
+
+        //Al final imprimimos el contenido de palabras, viendo que no permite duplicados
+        System.out.println("Palabras únicas:");
+        for (String p : palabras) {
+            System.out.println(p);
+        }
+
+
+    }
 }
